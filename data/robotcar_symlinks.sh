347e4eed23693530211478673f8d475f3d034ab0
Satuki717 <41312275+Satuki717@users.noreply.github.com>
2022-07-12 16:03:31 +0900

    edit robocar sh

--- data/robotcar_symlinks.sh
+++ data/robotcar_symlinks.sh
@@ -2,7 +2,7 @@
 
 set -x
 # change the directory
-ROBOTCAR_SDK_ROOT=/root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar-dataset-sdk
+ROBOTCAR_SDK_ROOT=/home/kai/data/code/AtLoc/data/robotcar-dataset-sdk
 
 ln -s ${ROBOTCAR_SDK_ROOT}/models/ /root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar_camera_models
 ln -s ${ROBOTCAR_SDK_ROOT}/python/ /root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar_sdk
